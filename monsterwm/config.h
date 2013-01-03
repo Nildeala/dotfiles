9a6d8ddb0941fa6e515e5bedf9ee77e93426f258
Schoewilliam <schoewilliam@gmail.com>
2013-01-03 19:06:08 +0400

    Some colors modifications and added separations to conky

--- monsterwm/config.h
+++ monsterwm/config.h
@@ -20,7 +20,7 @@
 #define FOLLOW_MOUSE    False     /* focus the window the mouse just entered */
 #define CLICK_TO_FOCUS  True      /* focus an unfocused window when clicked  */
 #define FOCUS_BUTTON    Button3   /* mouse button to be used along with CLICK_TO_FOCUS */
-#define BORDER_WIDTH    1         /* window border width */
+#define BORDER_WIDTH    2         /* window border width */
 #define FOCUS           "#ff950e" /* focused window border color    */
 #define UNFOCUS         "#444444" /* unfocused window border color  */
 #define MINWSZ          50        /* minimum window size in pixels  */
@@ -44,11 +44,13 @@ static const AppRule rules[] = { \
  * must always end with ', NULL };'
  */
 static const char *termcmd[] = { "urxvt",     NULL };
-static const char *menucmd[] = { "dmenu_run", "-fn", "-*-envypn-medium-*-*--13-*-*-*-*-*-*-1", "-nb", "#909090", "-nf", "#222", "-sb", "#1a1a1a", "-sf", "#ccc", "-l", "10", NULL };
+static const char *menucmd[] = { "dmenu_run", "-fn", "-*-envypn-medium-*-*--15-*-*-*-*-*-*-1", "-nb", "#2a2a2a", "-nf", "#ccc", "-sb", "#909090", "-sf", "#111", "-l", "10", NULL };
 static const char *volupcmd[] = { "amixer", "-c", "0", "set", "Master", "3+", "unmute", NULL };
 static const char *voldowncmd[] = { "amixer", "-c", "0", "set", "Master", "3-", "unmute", NULL };
 static const char *volmutecmd[] = { "amixer", "sset", "Master", "toggle", NULL };
 static const char *mpdplaycmd[] = { "ncmpcpp", "toggle", NULL };
+static const char *scrot[] = { "scrot", NULL };
+static const char *lockscreen[] = { "xautolock", "-locknow", NULL };
 
 #define DESKTOPCHANGE(K,N) \
     {  MOD1,             K,              change_desktop, {.i = N}}, \
@@ -64,10 +66,10 @@ static Key keys[] = {
     {  MOD4,             XK_q,          killclient,        {NULL}},
     {  MOD1,             XK_Right,      next_win,          {NULL}},
     {  MOD1,             XK_Left,       prev_win,          {NULL}},
-    {  MOD1,             XK_r,          resize_master,     {.i = -10}}, /* decrease size in px */
-    {  MOD1,             XK_s,          resize_master,     {.i = +10}}, /* increase size in px */
-    {  MOD1,             XK_l,          resize_stack,      {.i = -10}}, /* shrink   size in px */
-    {  MOD1,             XK_g,          resize_stack,      {.i = +10}}, /* grow     size in px */
+    {  MOD4,             XK_t,          resize_master,     {.i = -15}}, /* decrease size in px */
+    {  MOD4,             XK_r,          resize_master,     {.i = +15}}, /* increase size in px */
+    {  MOD4,             XK_d,          resize_stack,      {.i = -15}}, /* shrink   size in px */
+    {  MOD4,             XK_s,          resize_stack,      {.i = +15}}, /* grow     size in px */
     {  MOD1|CONTROL,     XK_Left,       rotate,            {.i = -1}},
     {  MOD1|CONTROL,     XK_Right,      rotate,            {.i = +1}},
     {  MOD1|SHIFT,       XK_h,          rotate_filled,     {.i = -1}},
@@ -84,15 +86,17 @@ static Key keys[] = {
     {  MOD1|CONTROL,     XK_r,          quit,              {.i = 0}}, /* quit with exit value 0 */
     {  MOD1|CONTROL,     XK_q,          quit,              {.i = 1}}, /* quit with exit value 1 */
     {  MOD4,             XK_Return,     spawn,             {.com = termcmd}},
-    {  MOD1,             XK_p,          spawn,             {.com = menucmd}},
+    {  MOD4,             XK_v,          spawn,             {.com = menucmd}},
     {  0,                0x1008ff13,    spawn,             {.com = volupcmd}}, /* Augmenter le volume  touche multimédia */
     {  0,                0x1008ff11,    spawn,             {.com = voldowncmd}}, /* réduire le volume */
     {  0,                0x1008ff12,    spawn,             {.com = volmutecmd}}, /* muet */
     {  0,                0x1008ff14,    spawn,             {.com = mpdplaycmd}}, /* play music via ncmpcpp */
-    {  MOD4,             XK_j,          moveresize,        {.v = (int []){   0,  25,   0,   0 }}}, /* move down  */
-    {  MOD4,             XK_k,          moveresize,        {.v = (int []){   0, -25,   0,   0 }}}, /* move up    */
-    {  MOD4,             XK_l,          moveresize,        {.v = (int []){  25,   0,   0,   0 }}}, /* move right */
-    {  MOD4,             XK_h,          moveresize,        {.v = (int []){ -25,   0,   0,   0 }}}, /* move left  */
+    {  0,                0xff61,        spawn,             {.com = scrot}}, /* take screenshot via scrot */
+    {  0,                0xff13,        spawn,             {.com = lockscreen}}, /* lockscreen */
+    {  MOD1,             XK_j,          moveresize,        {.v = (int []){   0,  25,   0,   0 }}}, /* move down  */
+    {  MOD1,             XK_k,          moveresize,        {.v = (int []){   0, -25,   0,   0 }}}, /* move up    */
+    {  MOD1,             XK_l,          moveresize,        {.v = (int []){  25,   0,   0,   0 }}}, /* move right */
+    {  MOD1,             XK_h,          moveresize,        {.v = (int []){ -25,   0,   0,   0 }}}, /* move left  */
     {  MOD4|SHIFT,       XK_j,          moveresize,        {.v = (int []){   0,   0,   0,  25 }}}, /* height grow   */
     {  MOD4|SHIFT,       XK_k,          moveresize,        {.v = (int []){   0,   0,   0, -25 }}}, /* height shrink */
     {  MOD4|SHIFT,       XK_l,          moveresize,        {.v = (int []){   0,   0,  25,   0 }}}, /* width grow    */
@@ -110,9 +114,8 @@ static Key keys[] = {
  * mouse shortcuts
  */
 static Button buttons[] = {
-    {  MOD1,    Button1,     mousemotion,   {.i = MOVE}},
-    {  MOD1,    Button3,     mousemotion,   {.i = RESIZE}},
-    {  MOD4,    Button3,     spawn,         {.com = menucmd}},
+    {  MOD4,    Button1,     mousemotion,   {.i = MOVE}},
+    {  MOD4,    Button3,     mousemotion,   {.i = RESIZE}},
 };
 #endif
 
