32dbf4bbc661cbc86589dac64de79434a0353779
Schoewilliam <schoewilliam@gmail.com>
2015-04-24 03:54:23 +0200

    Added i3lock directory

--- i3lock/i3lock_bind.sh
+++ i3lock/i3lock_bind.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Script to trigger a really nice looking lockscreen
+# 	Depends on: scrot, i3lock, imagemagick
+# 	Details & screenshot @ http://www.reddit.com/r/unixporn/comments/3358vu/i3lock_unixpornworthy_lock_screen/
+
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f $HOME/Images/icons/lock.png ]] && convert /tmp/screen.png $HOME/Images/icons/lock.png -gravity center -composite -matte /tmp/screen.png
